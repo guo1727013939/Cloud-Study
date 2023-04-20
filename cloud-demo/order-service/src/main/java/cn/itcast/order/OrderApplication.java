ff9167c63ef29f6f382aeb81718b0611b19467ac
国洪志 <guohongzhi@51ddi.com>
2023-04-21 00:26:02 +0800

    Nacos

--- cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -20,15 +20,9 @@ public class OrderApplication {
     @Bean
     @LoadBalanced
     //默认轮询实现类 ZoneAvoidanceRule 以区域可用的服务器为基础进行服务器的选择。
-    // 使用Zone对服务器进行分类，这个Zone可以理解为一个机房、一个机架等。而后再对Zone内的多个服务做轮询。
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
-    //第一种设置负载均衡 全局的配置方案，对所有服务都是随机
-    @Bean
-    public IRule randomRule(){
-        /*随机，实现IRule的实现类都可使用*/
-        return new RandomRule();
-    }
+
 
 }
